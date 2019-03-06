25e7b4a0e1500d313d81a2c4b30ecba508748046
Lúcás Meier <cronokirby@gmail.com>
2019-03-06 19:59:11 +0100

    Add function to build byte frequency from bytes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::collections::HashMap;
+
+fn build_byte_freqs<I : IntoIterator<Item=u8>>(bytes: I) -> HashMap<u8, i32> {
+    let mut acc = HashMap::new();
+    for b in bytes {
+        acc.insert(b, acc.get(&b).unwrap_or(&0) + 1);
+    }
+    acc
+}
+
 fn main() {
     println!("Hello, world!");
 }
