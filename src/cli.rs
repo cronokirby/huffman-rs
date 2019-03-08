17833b2f6b4de32e437431fd0a72452888435431
Lúcás Meier <cronokirby@gmail.com>
2019-03-08 18:25:43 +0100

    Use the same input file as the corpus

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io;
-use std::io::Read;
+use std::io::{Read, Seek};
 use crate::structopt::StructOpt;
 use crate::coding;
 
@@ -10,29 +10,20 @@ pub enum Opt {
     #[structopt(name = "encode")]
     /// Encode a file
     Encode {
-        /// The path of the corpus to use
-        /// 
-        /// Character frequency will be calculated from this file
-        /// by counting the occurrences of each character.
-        corpus: String,
         /// The input file to encode
         input: String,
         #[structopt(short = "o")]
         /// The output file to put the decoded text into
-        output: Option<String>
+        output: String
     },
     #[structopt(name = "decode")]
     /// Decode a file
     Decode {
-        /// The path of the corpus to use
-        /// 
-        /// This must match the corpus used to encode the file.
-        corpus: String,
         /// The input file to decode
         input: String,
         #[structopt(short = "o")]
         /// The output file to put the decoded text into
-        output: Option<String>
+        output: String
     }
 }
 
@@ -42,25 +33,21 @@ impl Opt {
     pub fn dispatch(self) -> io::Result<()> {
         match self {
             Opt::Decode { .. } => unimplemented!(),
-            Opt::Encode { corpus, input, output } => encode(corpus, input, output)
+            Opt::Encode { input, output } => encode(input, output)
         }
     }
 }
 
-fn encode(corpus: String, input: String, output: Option<String>) -> io::Result<()> {
-    let real_output = output.unwrap_or_else(|| {
-        let mut real = input.clone();
-        real.push_str(".out");
-        real
-    });
-    let corpus_file = File::open(corpus)?;
-    let input_file = File::open(input)?;
-    let output_file = File::create(real_output)?;
+fn encode(input: String, output: String) -> io::Result<()> {
+    let mut input_file = File::open(input)?;
+    let output_file = File::create(output)?;
     let mut output_writer = io::BufWriter::new(output_file);
 
-    let freqs = coding::build_byte_freqs(corpus_file.bytes())?;
+    let input_copy = input_file.try_clone()?;
+    let freqs = coding::build_byte_freqs(input_copy.bytes())?;
     let tree = coding::HuffTree::from_freqs(freqs);
     let mut encoder = coding::HuffWriter::from_tree(tree);
+    input_file.seek(io::SeekFrom::Start(0))?;
     for maybe_byte in input_file.bytes() {
         let byte = maybe_byte?;
         encoder.write_byte(byte, &mut output_writer)?;
